844651524d6578578df5b64b51a98deaf7ddfdb6
luisGuz12 <124893198+luisGuz12@users.noreply.github.com>
2023-02-09 07:55:38 -0600

    Add files via upload

---
+++ test.js
@@ -0,0 +1,7 @@
+"use strict";//configurar modo estricto
+let usename="Usnavi"
+ console.log(usename)
+
+
+let userName="Luis" //asi declaramos la variable 
+console.log(`el usuario es: ${userName}`) 
